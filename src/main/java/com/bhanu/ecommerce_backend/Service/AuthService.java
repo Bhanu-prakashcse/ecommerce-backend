4ab8c60964871175898806c17c8a483668f9b8b9
Bhanu-prakashcse <jobsforbhanu13@example.com>
2025-07-22 15:55:00 +0530

    Final fix: register with email, login with username only

--- src/main/java/com/bhanu/ecommerce_backend/Service/AuthService.java
+++ src/main/java/com/bhanu/ecommerce_backend/Service/AuthService.java
@@ -23,6 +23,7 @@ public class AuthService {
     public AuthResponse register(AuthRequest request, Role role) {
         User user=User.builder()
                 .username(request.getUsername())
+                .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(role)
                 .build();
