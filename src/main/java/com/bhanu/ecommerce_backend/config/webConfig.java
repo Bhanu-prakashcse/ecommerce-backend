405d147859ad623059fe9cc36bea95cad48c9711
Bhanu-prakashcse <jobsforbhanu13@example.com>
2025-07-22 22:26:15 +0530

    Updated new domain from frontend

--- src/main/java/com/bhanu/ecommerce_backend/config/webConfig.java
+++ src/main/java/com/bhanu/ecommerce_backend/config/webConfig.java
@@ -14,7 +14,7 @@ public class webConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://brilliant-crisp-ab467d.netlify.app")
+                        .allowedOrigins("http://localhost:3000", "https://ecomhub-spring.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization") // Optional
